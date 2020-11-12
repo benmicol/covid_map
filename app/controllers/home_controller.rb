ef797fbc8c5982f5d81eb58afd32e9f5f77253a3
Ben Micol <benmicol@gmail.com>
2020-11-12 15:50:16 -0500

    switched API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,19 @@
 class HomeController < ApplicationController
     skip_before_action :verify_authenticity_token
     def index
+        escaped_address = URI.escape("https://covid19-api.org/api/status") 
+        uri = URI.parse(escaped_address)
+        response = Net::HTTP.get(uri)
+		parsed_response = JSON.parse(response)
+		parsed_response.each do |hash| CoronaDatum.find_by(country: hash['country']).update(confirmed_cases: hash['cases']) end
         @data = CoronaDatum.pluck(:country, :confirmed_cases)
     end
+
     def receive_data
         data = request.body.read
         if data.present?
             data = JSON.parse(data)
-            data['Countries'].each do |hash| CoronaDatum.find_by(country: hash['CountryCode']).update(confirmed_cases: hash['TotalConfirmed']) end
+            data.each do |hash| CoronaDatum.find_by(country: hash['country']).update(confirmed_cases: hash['cases']) end
             render :json => {:status => 200}
         else
             render :json => {:status => 404}
--- db/migrate/20201110222854_drop_data.rb
+++
@@ -1,5 +0,0 @@
-class DropData < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :corona_data
-  end
-end
